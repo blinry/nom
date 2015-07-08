f801e70954c9a4a03aeb009c50c10c500e16e883
Sebastian Morr <sebastian@morr.cc>
2015-07-08 17:53:18 +0200

    Wrap the module Nom around library code

--- lib/nom/food_entry.rb
+++ lib/nom/food_entry.rb
@@ -1,21 +1,23 @@
-class FoodEntry
-    attr_reader :date, :kcal, :description
+module Nom
+    class FoodEntry
+        attr_reader :date, :kcal, :description
 
-    def self.from_line line
-        date, kcal, description = line.split(" ", 3)
-        date = Date.parse(date)
-        kcal = kcal.to_i
-        description.chomp!
-        FoodEntry.new(date, kcal, description)
-    end
+        def self.from_line line
+            date, kcal, description = line.split(" ", 3)
+            date = Date.parse(date)
+            kcal = kcal.to_i
+            description.chomp!
+            FoodEntry.new(date, kcal, description)
+        end
 
-    def initialize date, kcal, description
-        @date = date
-        @kcal = kcal
-        @description = description
-    end
+        def initialize date, kcal, description
+            @date = date
+            @kcal = kcal
+            @description = description
+        end
 
-    def to_s
-        "#{@date} #{@kcal} #{@description}\n"
+        def to_s
+            "#{@date} #{@kcal} #{@description}\n"
+        end
     end
 end
--- lib/nom/weight_database.rb
+++ lib/nom/weight_database.rb
@@ -1,120 +1,122 @@
 require "yaml"
 require "fileutils"
 
-class WeightDatabase
-    def initialize(file)
-        @interpolated = {}
-        @weights = {}
-        @moving_averages = {}
+module Nom
+    class WeightDatabase
+        def initialize(file)
+            @interpolated = {}
+            @weights = {}
+            @moving_averages = {}
 
-        FileUtils.touch(file)
-        IO.readlines(file).each do |line|
-            date, weight = line.split(" ", 2)
-            date = Date.parse(date)
-            @weights[date] = weight.to_f
-            @interpolated[date] = false
+            FileUtils.touch(file)
+            IO.readlines(file).each do |line|
+                date, weight = line.split(" ", 2)
+                date = Date.parse(date)
+                @weights[date] = weight.to_f
+                @interpolated[date] = false
+            end
         end
-    end
 
-    def interpolate_gaps!
-        @weights.keys.each_cons(2) do |a, b|
-            (a+1).upto(b-1) do |d|
-                @weights[d] = @weights[a] + (@weights[a]-@weights[b])/(a-b)*(d-a)
+        def interpolate_gaps!
+            @weights.keys.each_cons(2) do |a, b|
+                (a+1).upto(b-1) do |d|
+                    @weights[d] = @weights[a] + (@weights[a]-@weights[b])/(a-b)*(d-a)
+                    @interpolated[d] = true
+                end
+            end
+        end
+
+        def precompute_moving_average!(alpha, beta, goal, rate)
+            trend = dampened_rate(@weights[first], goal, rate)/7.0
+
+            @moving_averages[first] = at(first)
+            (first+1).upto(last).each do |d|
+                @moving_averages[d] = alpha*at(d) + (1-alpha)*(@moving_averages[d-1]+trend)
+                trend = beta*(@moving_averages[d]-@moving_averages[d-1]) + (1-beta)*trend
+            end
+        end
+
+        def predict_weights!(rate, goal, tail)
+            d = (last)
+            loop do
+                if (@weights[d] - goal).abs < 0.1
+                    tail -= 1
+                end
+                if tail == 0
+                    break
+                end
+
+                d += 1
+                prev_weight = @moving_averages[d-1] || @weights[d-1]
+                @weights[d] = prev_weight+dampened_rate(prev_weight, goal, rate)/7.0
                 @interpolated[d] = true
             end
         end
-    end
 
-    def precompute_moving_average!(alpha, beta, goal, rate)
-        trend = dampened_rate(@weights[first], goal, rate)/7.0
-
-        @moving_averages[first] = at(first)
-        (first+1).upto(last).each do |d|
-            @moving_averages[d] = alpha*at(d) + (1-alpha)*(@moving_averages[d-1]+trend)
-            trend = beta*(@moving_averages[d]-@moving_averages[d-1]) + (1-beta)*trend
-        end
-    end
-
-    def predict_weights!(rate, goal, tail)
-        d = (last)
-        loop do
-            if (@weights[d] - goal).abs < 0.1
-                tail -= 1
+        def dampened_rate weight, goal, rate
+            r = (goal-weight).to_f
+            if r.abs > 1
+                r/r.abs*rate
+            else
+                r*rate
             end
-            if tail == 0
-                break
+        end
+
+        def real? date
+            @weights[date] and not @interpolated[date]
+        end
+
+        def at date
+            @weights[date]
+        end
+
+        def moving_average_at date
+            @moving_averages[date]
+        end
+
+        def rate_at date, goal, rate
+            if date > last_real
+                dampened_rate(@weights[date], goal, rate)
+            else
+                dampened_rate(@moving_averages[date], goal, rate)
             end
-
-            d += 1
-            prev_weight = @moving_averages[d-1] || @weights[d-1]
-            @weights[d] = prev_weight+dampened_rate(prev_weight, goal, rate)/7.0
-            @interpolated[d] = true
         end
-    end
 
-    def dampened_rate weight, goal, rate
-        r = (goal-weight).to_f
-        if r.abs > 1
-            r/r.abs*rate
-        else
-            r*rate
+        def empty?
+            @weights.empty?
         end
-    end
 
-    def real? date
-        @weights[date] and not @interpolated[date]
-    end
-
-    def at date
-        @weights[date]
-    end
-
-    def moving_average_at date
-        @moving_averages[date]
-    end
-
-    def rate_at date, goal, rate
-        if date > last_real
-            dampened_rate(@weights[date], goal, rate)
-        else
-            dampened_rate(@moving_averages[date], goal, rate)
+        def first
+            @weights.keys.min
         end
-    end
 
-    def empty?
-        @weights.empty?
-    end
+        def last
+            @weights.keys.max
+        end
 
-    def first
-        @weights.keys.min
-    end
+        def last_real
+            @interpolated.select{|d, i| not i}.keys.max
+        end
 
-    def last
-        @weights.keys.max
-    end
+        def truncate date
+            @weights.delete_if{|d, w| d < date}
+        end
 
-    def last_real
-        @interpolated.select{|d, i| not i}.keys.max
-    end
+        def min
+            @weights.values.min
+        end
 
-    def truncate date
-        @weights.delete_if{|d, w| d < date}
-    end
+        def max
+            @weights.values.max
+        end
 
-    def min
-        @weights.values.min
-    end
-
-    def max
-        @weights.values.max
-    end
-
-    def find_gap days
-        gap = @weights.keys.reverse.each_cons(2).find{|a,b| a-b > days}
-        if gap
-            gap.reverse
-        else
-            nil
+        def find_gap days
+            gap = @weights.keys.reverse.each_cons(2).find{|a,b| a-b > days}
+            if gap
+                gap.reverse
+            else
+                nil
+            end
         end
     end
 end
